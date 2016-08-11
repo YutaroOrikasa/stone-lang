ca33f7a04b9b7f15bcff63cddc16d8aacaab2be9
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2016-08-12 08:34:26 +0900

    Add forgetted file (src/stone/_parsers/NestedEnvironment.java)
    the file is essential for function feature
    so the codes that have function feature before this commit are not work

---
+++ src/stone/_parsers/NestedEnvironment.java
@@ -0,0 +1,38 @@
+package stone._parsers;
+
+import stone.BasicEnvironment;
+import stone.Environment;
+
+class NestedEnvironment implements Environment {
+
+	Environment outerEnv;
+	Environment innerEnv;
+
+	public NestedEnvironment(Environment outerEnv) {
+		this(outerEnv, new BasicEnvironment());
+
+	}
+
+	public NestedEnvironment(Environment outerEnv, Environment innerEnv) {
+		this.outerEnv = outerEnv;
+		this.innerEnv = innerEnv;
+	}
+
+	@Override
+	public void put(String name, Object value) {
+		innerEnv.put(name, value);
+
+	}
+
+	@Override
+	public Object get(String name) {
+		Object value = innerEnv.get(name);
+
+		if (value == null) {
+			value = outerEnv.get(name);
+		}
+
+		return value;
+	}
+
+}
