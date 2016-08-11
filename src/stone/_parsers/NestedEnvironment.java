6e62cc6aa3496033d9bdeb5f99d9f3ca2eebb695
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2016-08-12 08:01:12 +0900

    Add new constructor (stone._parsers.NestedEnvironment)
    and change field and argument name

--- src/stone/_parsers/NestedEnvironment.java
+++ src/stone/_parsers/NestedEnvironment.java
@@ -5,25 +5,31 @@ import stone.Environment;
 
 class NestedEnvironment implements Environment {
 
+	Environment outerEnv;
 	Environment innerEnv;
-	BasicEnvironment outerEnv = new BasicEnvironment();
 
-	public NestedEnvironment(Environment innerEnv) {
+	public NestedEnvironment(Environment outerEnv) {
+		this(outerEnv, new BasicEnvironment());
+
+	}
+
+	public NestedEnvironment(Environment outerEnv, Environment innerEnv) {
+		this.outerEnv = outerEnv;
 		this.innerEnv = innerEnv;
 	}
 
 	@Override
 	public void put(String name, Object value) {
-		outerEnv.put(name, value);
+		innerEnv.put(name, value);
 
 	}
 
 	@Override
 	public Object get(String name) {
-		Object value = outerEnv.get(name);
+		Object value = innerEnv.get(name);
 
 		if (value == null) {
-			value = innerEnv.get(name);
+			value = outerEnv.get(name);
 		}
 
 		return value;
