1251ef442d6c0dea23982c8e66dc2e28096bd1ff
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2016-08-15 18:05:17 +0900

    Make StoneException can wrap exception (stone.StoneException)

--- src/stone/StoneException.java
+++ src/stone/StoneException.java
@@ -3,14 +3,21 @@ package stone;
 import stone.ast.ASTree;
 
 public class StoneException extends RuntimeException {
-	
+
 	public StoneException(String msg) {
 		super(msg);
 	}
-	
+
 	public StoneException(String msg, ASTree t) {
 		super(msg + " " + t.location());
 	}
 
+	public StoneException(Throwable cause, ASTree t) {
+		super(t.location(), cause);
+	}
+
+	public StoneException(Throwable cause) {
+		super(cause);
+	}
 
 }
