4133e81c7c1534216e7ec5c0b5cc9d79d23dd92a
orikasa <orikasa@csg.ci.i.u-tokyo.ac.jp>
2016-08-15 15:36:51 +0900

    Make if and while condition expr work well (stone._types.Utility.convertToStoneBooleanValue(Object))
        if 1 < -1 { 1 }
    before:
        eval: 1
    after:
        eval: 0

--- src/stone/_types/Utility.java
+++ src/stone/_types/Utility.java
@@ -3,10 +3,12 @@ package stone._types;
 public class Utility {
 
 	public static boolean convertToStoneBooleanValue(Object condObject) {
-		// 数値の0であるならば偽
+		// Integerの0であるならば偽
+		// またはBooleanのfalseならば偽
 		// またはnilなら偽
 		// それ以外なら真
-		return !((condObject instanceof Integer && (int) condObject == 0) || condObject == Nil.STONE_NIL);
+		return !((condObject instanceof Integer && (int) condObject == 0)
+				|| (condObject instanceof Boolean && (boolean) condObject == false) || condObject == Nil.STONE_NIL);
 	}
 
 }
